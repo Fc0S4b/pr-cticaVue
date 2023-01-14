8704f7f709cae5648a1730e18570de3f53f57d00
fc0S4b <fcoj.sabugo@gmail.com>
2023-01-14 19:30:18 -0300

    attribute-binding in a file

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,12 @@
 <script>
-export default{
-  data(){
-    return{
-      message: 'Hello world',
-      isRed: true,
-      color: 'green'
-    }
-  },
-  methods:{
-    toggleRed(){
-      this.isRed = !this.isRed
-    },
-    toggleColor(){
-      this.color = this.color === 'green' ? 'blue':'green'
-    }
-  }
-}
+
 </script>
 
 <template>
-<p>
-  <span :title="message">
-    Hello
-  </span>
-</p>
-
-<p :class="{red: isRed}" @click="toggleRed">
-  hello 2
-</p>
-
-<p :style="{color}" @click='toggleColor'>
-  hello 3
-</p>
 
 </template>
 
 
 <style>
-.red{
-  color: red;
-}
+
 </style>
